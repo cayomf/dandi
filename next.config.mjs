de72838ca159d70765d5e1f3f96df2319cf0b560
Cayo Cesar <cayomf@gmail.com>
2024-10-22 05:29:24 -0300

    ajustes deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+   /** @type {import('next').NextConfig} */
+   const nextConfig = {
+    reactStrictMode: true,
+    transpilePackages: ['@heroicons/react'],
+  }
 
-export default nextConfig;
+  module.exports = nextConfig
