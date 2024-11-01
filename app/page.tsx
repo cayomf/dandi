adf48e4624f0d0a5dfa16a712fac6237afda7f66
Cayo Cesar <cayomf@gmail.com>
2024-11-01 02:06:49 -0300

    fix: error deploy vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React from 'react';
 import Image from "next/image";
-import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react"
 import { useRouter } from 'next/navigation'
 
